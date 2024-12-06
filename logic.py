aae6caf18dbc9ee007ca88d21531cacd9f5f21c5
ELot.py <125884479+GodsplanElot@users.noreply.github.com>
2024-12-06 09:12:22 +0100

    updated the logic text

--- logic.py
+++ logic.py
@@ -5,6 +5,12 @@ def guess (x):
     random_number = random.randint(1, x)
     guess = 0
     while (guess != random_number):
-        guess = input(f'Guess a number between 1 and {x}: ')
-        print(guess)
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print('sorry, guess again. too low.')
+        elif guess > random_number:
+            print('sorry, guess again. too high.')
+    print(f'yay, congrats. You have guessed the number {random_number} correctly')
+    print('*****DONE*****')
+guess(10)
 
